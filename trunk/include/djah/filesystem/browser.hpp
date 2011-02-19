f96b58df2bbfeb41cdf7d287902e747069435eb1
osheun <osheun@eee9f6e4-4589-4ce0-aba5-afd3fc070d3c>
2011-02-19 04:05:23 +0000

    filsesystem include directory

---
+++ trunk/include/djah/filesystem/browser.hpp
@@ -0,0 +1,40 @@
+#ifndef DJAH_FILESYSTEM_BROWSER_HPP
+#define DJAH_FILESYSTEM_BROWSER_HPP
+
+#include <set>
+#include "../utils/singleton.hpp"
+#include "source.hpp"
+#include "stream.hpp"
+
+namespace djah { namespace filesystem {
+
+	class source;
+
+	class browser
+		: public utils::singleton<browser>
+	{
+		DJAH_MAKE_SINGLETON(browser);
+
+	public:
+
+		void addLoadingChannel(source *src);
+		void addSavingChannel(source *src);
+
+		stream_ptr openReadStream(const std::string &url);
+		stream_ptr openWriteStream(const std::string &url);
+
+	private:
+
+		~browser();
+
+		// Predicate used to compare 2 sources
+		struct source_comp { bool operator ()(const source *lhs, const source *rhs) { return *lhs < *rhs; } };
+		typedef std::multiset<source*, source_comp> source_list_t;
+
+		source_list_t loading_channels_;
+		source_list_t saving_channels_;
+	};
+
+} /*filesystem*/ } /*djah*/
+
+#endif /* DJAH_FILESYSTEM_BROWSER_HPP */
