26710ed95907eb7d4b4e24c0d17b401d6a5e7b42
osheun <osheun@eee9f6e4-4589-4ce0-aba5-afd3fc070d3c>
2011-02-19 04:05:57 +0000

    filsesystem source directory

---
+++ trunk/src/filesystem/browser.cpp
@@ -0,0 +1,78 @@
+#include "filesystem/browser.hpp"
+#include "filesystem/source.hpp"
+
+namespace djah { namespace filesystem {
+
+	//----------------------------------------------------------------------------------------------
+	browser::~browser()
+	{
+		source_list_t::iterator it;
+		source_list_t::iterator it_end = loading_channels_.end();
+		for(it = loading_channels_.begin(); it != it_end; ++it)
+			delete *it;
+
+		it_end = saving_channels_.end();
+		for(it = saving_channels_.begin(); it != it_end; ++it)
+			delete *it;
+	}
+	//----------------------------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------------------------
+	void browser::addLoadingChannel(source *src)
+	{
+		loading_channels_.insert(src);
+	}
+	//----------------------------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------------------------
+	void browser::addSavingChannel(source *src)
+	{
+		saving_channels_.insert(src);
+	}
+	//----------------------------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------------------------
+	stream_ptr browser::openReadStream(const std::string &url)
+	{
+		stream_ptr strm;
+
+		source_list_t::iterator it;
+		source_list_t::iterator it_end = loading_channels_.end();
+		for(it = loading_channels_.begin(); it != it_end; ++it)
+		{
+			if( (*it)->isFetchable(url) )
+			{
+				strm = (*it)->loadStream(url);
+				break;
+			}
+		}
+
+		return strm;
+	}
+	//----------------------------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------------------------
+	stream_ptr browser::openWriteStream(const std::string &url)
+	{
+		stream_ptr strm;
+
+		source_list_t::iterator it;
+		source_list_t::iterator it_end = saving_channels_.end();
+		for(it = saving_channels_.begin(); it != it_end; ++it)
+		{
+			if( (*it)->isWritable(url) )
+			{
+				strm = (*it)->saveStream(url);
+				break;
+			}
+		}
+
+		return strm;
+	}
+	//----------------------------------------------------------------------------------------------
+
+} /*filesystem*/ } /*djah*/
