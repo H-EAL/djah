41f7fa8a8f933211651a6899832b99aef37e9d2a
osheun <osheun@eee9f6e4-4589-4ce0-aba5-afd3fc070d3c>
2011-08-15 02:09:54 +0000

    URL class now works properly

---
+++ trunk/sources/filesystem/url.cpp
@@ -0,0 +1,73 @@
+#include "filesystem/url.hpp"
+
+namespace djah { namespace filesystem {
+
+	//----------------------------------------------------------------------------------------------
+	url::url(const std::string &urlStr)
+		: url_str_(urlStr)
+	{
+		parse();
+	}
+	//----------------------------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------------------------
+	url::~url()
+	{
+	}
+	//----------------------------------------------------------------------------------------------
+	
+	//----------------------------------------------------------------------------------------------
+	const std::string url::toString() const
+	{
+		return protocol_
+			+ (protocol_.empty() ? "" : "://")
+			+ branch_path_
+			+ (branch_path_.empty() ? "" : "/")
+			+ file_name_
+			+ (file_extension_.empty() ? "" : ".")
+			+ file_extension_; 
+	}
+	//----------------------------------------------------------------------------------------------
+
+
+
+	//----------------------------------------------------------------------------------------------
+	void url::parse()
+	{
+		// protocol:///branch/.../path/fileName.extension
+
+		const size_t protocolEndsAt = url_str_.find("://");
+
+		size_t branchPathBeginsAt = 0;
+		size_t fileNameBeginsAt = 0;
+
+		if( protocolEndsAt != std::string::npos )
+		{
+			// Protocol found
+			protocol_ = url_str_.substr(0, protocolEndsAt);
+			branchPathBeginsAt = url_str_.find_first_not_of("/\\", protocolEndsAt+1);
+			fileNameBeginsAt = branchPathBeginsAt+1;
+		}
+
+		size_t branchPathEndsAt = url_str_.find_last_of("/\\");
+		if( branchPathEndsAt != std::string::npos && branchPathEndsAt > branchPathBeginsAt )
+		{
+			// Branch path found
+			branch_path_ = url_str_.substr(branchPathBeginsAt, branchPathEndsAt - branchPathBeginsAt);
+			fileNameBeginsAt = branchPathEndsAt+1;
+		}
+
+		const size_t extensionBeginsAt = url_str_.find_last_of('.');
+
+		file_name_ = url_str_.substr(fileNameBeginsAt, extensionBeginsAt - fileNameBeginsAt);
+
+		if( extensionBeginsAt != std::string::npos )
+		{
+			// Extension found
+			file_extension_ = url_str_.substr(extensionBeginsAt+1);
+		}
+	}
+	//----------------------------------------------------------------------------------------------
+
+} /*filesystem*/ } /*djah*/
