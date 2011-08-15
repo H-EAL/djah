349016c9f7863f537d6d20ac2d62319b39125bfe
Houssem Achouri <H-EAL@users.noreply.github.com>
2011-08-15 02:09:54 +0000

    URL class now works properly

--- include/djah/filesystem/url.hpp
+++ include/djah/filesystem/url.hpp
@@ -1,36 +1,43 @@
-#ifndef DJAH_FILESYSTEM_URI_HPP
-#define DJAH_FILESYSTEM_URI_HPP
+#ifndef DJAH_FILESYSTEM_URL_HPP
+#define DJAH_FILESYSTEM_URL_HPP
 
 #include <string>
 
 namespace djah { namespace filesystem {
 
-	class uri
+	class url
 	{
 	public:
 
-		uri(const std::string &uriStr);
-		~uri();
+		url(const std::string &urlStr);
+		~url();
 
-		const std::string& str()			const { return uri_str_;			}
-		const std::string& completeStr()	const { return complete_uri_str_;	}
+		const std::string& str()			const { return url_str_;			}
 		const std::string& protocol()		const { return protocol_;			}
 		const std::string& branchPath()		const { return branch_path_;		}
 		const std::string& fileName()		const { return file_name_;			}
 		const std::string& fileExtension()	const { return file_extension_;		}
+		const std::string fileNameWithExtension()	const
+		{ return file_name_ + "." + file_extension_; }
 
-		const uri operator =(const uri &rhs);
+		const std::string toString() const;
+		url& operator =(const std::string &urlStr) { *this = url(urlStr); return (*this); }
+		
+	private:
+
+		void parse();
 
 	private:
 
-		std::string uri_str_;
-		std::string complete_uri_str_;
+		std::string url_str_;
 		std::string protocol_;
 		std::string branch_path_;
 		std::string file_name_;
 		std::string file_extension_;
 	};
 
+	const url operator /(const url &lhs, const url &rhs);
+
 } /*filesystem*/ } /*djah*/
 
-#endif /* DJAH_FILESYSTEM_URI_HPP */
+#endif /* DJAH_FILESYSTEM_URL_HPP */
